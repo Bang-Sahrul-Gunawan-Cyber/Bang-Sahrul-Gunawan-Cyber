da4fe770cee7ca5e13d43780d8628a64dd629dee
Sahrul Gunawan <81924782+Bang-Sahrul-Gunawan-Cyber@users.noreply.github.com>
2021-05-01 01:21:29 +0700

    Add files via upload

---
+++ fix-bug-gem-metasploit.sh
@@ -0,0 +1,30 @@
+# FIXING INSTALLISASI METASPLOIT ERROR GEM SOLUSI
+
+# DILARANG KERAS RECORD TOOLS GW BANGSAT
+
+
+
+
+
+
+
+
+
+
+
+gem install simplecov
+gem install redcarpet
+gem install yard
+gem install pry-byebug
+gem install octokit
+gem install memory_profiler
+gem install ruby-prof
+gem install factory_bot_rails
+# Make rspec output shorter and more  >
+gem install fivemat
+gem install rspec-rails
+gem install rspec-rerun
+gem install rubocop
+gem install swagger-blocks
+gem install timecop
+bundler --system
